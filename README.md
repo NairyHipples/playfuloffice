93a8cab524c77be3259994c4c7c3229280d6247b
Jonatan Östling <jonatan.ostling@gmail.com>
2016-10-08 15:02:44 +0200

    Another local CSS update.
    Added the blue and white striped banner, changed fonts, got the nav
    working correctly, changed backgrounds and content on pages.

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# playfuloffice
-Website for the book "Playful Office".
+# chronus
+Chronus official website
--- app/app.js
+++ app/app.js
@@ -28,15 +28,8 @@ app.config(['$routeProvider', '$locationProvider',
 var controllers = {};
 controllers.searchController = function($scope) {
   $scope.books = [
-    { name: 'J.R.R. Tolkien', title: 'The Hobbit', img: 'img/books/hobbit.jpg'},
-    { name: 'Harper Lee', title: 'To Kill A Mockingbird', img: 'img/books/mockingbird.jpg'},
-    { name: 'Scott Fitzgerald', title: 'The Great Gatsby', img: 'img/books/gatsby.jpg'},
-    { name: 'Joseph Heller', title: 'Catch-22', img: 'img/books/22.jpg'},
-    { name: 'Anthony Burgess', title: 'A Clockwork Orange', img: 'img/books/clockwork.jpg'},
-    { name: 'Dr. Seuss', title: 'The Cat in the Hat', img: 'img/books/cat.png'},
-    { name: 'Chris Mould', title: 'Something Wickedly Weird', img: 'img/books/wicked.jpg'},
-    { name: 'George R.R. Martin', title: 'A Game of Thrones', img: 'img/books/thrones.jpg'},
-    { name: 'J.D. Salinger', title: 'The Catcher in the Rye', img: 'img/books/rye.jpg' }
+    { name: 'Samuel West & Niklas Madsen', title: 'Playful Office', img: 'img/books/clockwork.jpg'}
+
   ];
 };
 
