c0b7cdb7eb9b28ccc7f8ff2cc502890d848ccb02
Jonatan Östling <jonatan.ostling@gmail.com>
2016-10-10 18:56:13 +0200

    Problems with the branch, trying to update.

--- scripts/main.js
+++ scripts/main.js
@@ -2,9 +2,6 @@
 
 $(document).ready(function() {
   //Jag har provat en massa funktioner som jag lämnar kvar för att komma ihåg i framtiden
-  $('.page-top').click(function(){
-    $('html, body').animate({ scrollTop: 0 }, 'fast');
-  });
   $('body').click(function(){
     $('.navHeaderCollapse').collapse('hide');
   });
